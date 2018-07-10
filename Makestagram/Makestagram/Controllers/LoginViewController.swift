e30ecd3281ef419e4604d7a91842ed2fa627dc49
Nathan Pillai <natepill@gmail.com>
2018-07-10 10:16:09 -0700

    Makestagram
    Instagram clone during MakeSchool

---
+++ Makestagram/Makestagram/Controllers/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Nathan Pillai on 7/9/18.
+//  Copyright © 2018 Nathan Pillai. All rights reserved.
+//
+
+
+import UIKit
+
+
+class LoginViewController: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        print("pressed")
+    }
+    
+    
+    
+}
